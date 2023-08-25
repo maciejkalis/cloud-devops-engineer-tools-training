50d58cf4e7089cb99d7228b5a5790f06abf6d7e0
Maciej Kaliś <mackal1993@gmail.com>
2023-08-25 18:31:26 +0200

    Testing docker agent

---
+++ Jenkins/First Training/Chapter 4/ssh-agent/src/test/java/com/learningjenkins/AppTest.java
@@ -0,0 +1,16 @@
+package com.learningjenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void CheckHelloWorld() {
+        App app = new App();
+        assertEquals("Hello World!", app.main());
+    }
+}
