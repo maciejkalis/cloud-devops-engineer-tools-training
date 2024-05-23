61bb4581a4d0ef30105e3e12e71c872d6dd3fddd
maciej.kalis <maciej.kalis@accenture.com>
2024-05-23 15:26:49 +0200

    Small edit

--- Terraform/gcp_skills_boost_training/training1/main.tf
+++ Terraform/gcp_skills_boost_training/training1/main.tf
@@ -8,6 +8,7 @@ resource "google_compute_network" "vpc_network" {
   mtu                     = 1460
 }
 
+# [Adding firewall rule]
 resource "google_compute_firewall" "rules" {
   project = var.project_id
   name    = "my-firewall-rule"
